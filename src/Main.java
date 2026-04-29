7a8d5cf75e7dfe0c593b3b27876911badd2945c4
angel¿ <155915830+NopAngel@users.noreply.github.com>
2026-04-29 00:20:37 -0400

    **

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import javax.swing.SwingUtilities;
 
 public class Main {
+    
     public static void main(String[] args) {
+
+        
         SwingUtilities.invokeLater(() -> {
             MainWindow mainWindow = new MainWindow();
             mainWindow.setVisible(true);
         });
+
+        
     }
+    
 }
